c1dcfdee3d66b55f18e0db691084ee8fa14aaee2
Vasile <vzavizion@yahoo.com>
2022-02-19 17:06:43 -0500

    docker file

--- AzureApp31/obj/Debug/netcoreapp3.1/AzureApp31.AssemblyInfo.cs
+++ AzureApp31/obj/Debug/netcoreapp3.1/AzureApp31.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("20777831-a49d-4608-a4de-3c5de37ac861")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureApp31")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
